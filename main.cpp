68c7213d316bc9c15a163ff3246c05a8c1ea33ac
AnlieJams <98762736+AnlieJams@users.noreply.github.com>
2023-05-26 19:17:08 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;    
+class Fraction {
+private:
+    int m_numerator;
+    int m_denominator;
+
+public:
+    Fraction() : m_numerator(0), m_denominator(1) {}
+    Fraction(int numerator, int denominator) : m_numerator(numerator), m_denominator(denominator) {}
+
+    friend std::ostream& operator << (std::ostream& out, const Fraction& f1)
+    {
+        out << f1.m_numerator << "/" << f1.m_denominator;
+        return out;
+    }
+};
+
+void printFraction(const Fraction* const ptr)
+{
+    if (ptr)
+    {
+       cout << *ptr << endl;
+    }
+}
+
+int main() {
+    auto ptr = std::unique_ptr<Fraction>(new Fraction(4, 5));
+    printFraction(ptr.get());
+
+    Fraction* ptr2 = ptr.get();
+    printFraction(ptr2);
+}
